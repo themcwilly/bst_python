65389ba847fbefe651494d2c1a400d9ad2f434d1
Brett <brettmcwilliams@gmail.com>
2020-03-18 11:15:52 -0500

    initial upload

---
+++ functions/circle.py
@@ -0,0 +1,3 @@
+import math
+def area(radius=1):
+    return math.pi*math.pow(radius,2)
---
+++ main.py
@@ -0,0 +1,13 @@
+import functions.circle as circ
+import os
+import pandas as pd
+
+if __name__ == '__main__':
+    r = 5
+    area = circ.area(radius=r)
+    print('The area of a circle with radius %f is %f'%(r,area))
+
+    abs_path = os.path.abspath(__file__ + "/../_data/data.csv")
+    data = pd.read_csv(abs_path)
+    data['area'] = data.apply(lambda x: circ.area(x['radius']), axis=1)
+    print()
